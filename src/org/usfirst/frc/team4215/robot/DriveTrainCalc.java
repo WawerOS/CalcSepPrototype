991f8addd66b1c22187cc640fdd6186bef2e191a
WawerOS <lion@waposta.com>
2015-07-19 09:19:07 -0500

    finished DrivetrainCalc

--- src/org/usfirst/frc/team4215/robot/DriveTrainCalc.java
+++ src/org/usfirst/frc/team4215/robot/DriveTrainCalc.java
@@ -2,7 +2,7 @@ package org.usfirst.frc.team4215.robot;
 
 public class DriveTrainCalc {
 
-	public double[] mecanumAlogorithm(double xMove, double yMove, double zMove){
+	public double[] mecanumAlgorithm(double xMove, double yMove, double zMove){
 		/*
 		 * Algorithm based on a whitepaper found at 
 		 * http://thinktank.wpi.edu/resources/346/ControllingMecanumDrive.pdf
@@ -10,18 +10,27 @@ public class DriveTrainCalc {
 		double speedOfTranslation = Math.sqrt(Math.pow(xMove,2)+Math.pow(yMove,2));
 		double angleOfTranslation = Math.asin((yMove/speedOfTranslation));
 		double rateOfRotation = zMove;
-		double biggestValue;
+		double biggestValue = 3; // to always be smaller then the first value
 		double[] voltageMultipliers = { 0, 0, 0, 0,};
 		
 		voltageMultipliers[0] = Math.sin(angleOfTranslation+Math.PI/4)*speedOfTranslation+rateOfRotation;
 		voltageMultipliers[1] = Math.cos(angleOfTranslation+Math.PI/4)*speedOfTranslation-rateOfRotation;
 		voltageMultipliers[2] = Math.cos(angleOfTranslation+Math.PI/4)*speedOfTranslation+rateOfRotation;
 		voltageMultipliers[3] = Math.sin(angleOfTranslation+Math.PI/4)*speedOfTranslation-rateOfRotation;
+		
+		//normalization algorithm from paper referenced above
 		for(int i = 0; i > 3;i++){
-			if(voltageMultipliers[i] > 1 && voltageMultpliers){
-				biggest
+			if(Math.abs(voltageMultipliers[i]) > 1 || 
+					Math.abs(voltageMultipliers[i]) > Math.abs(biggestValue)){
+				biggestValue = voltageMultipliers[i];
+			}
+		}
+		if(biggestValue == 3){
+			for(int j = 0; j > 3; j++){
+			voltageMultipliers[j] /= biggestValue;
 			}
 		}
 		return voltageMultipliers;
 	}
+	
 }
