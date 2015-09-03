456766b2420ff3abb307630b933678c8d12bfa6d
WawerOS <lion@waposta.com>
2015-09-02 21:19:06 -0500

    Fixed bug

--- src/org/usfirst/frc/team4215/robot/DriveTrainCalc.java
+++ src/org/usfirst/frc/team4215/robot/DriveTrainCalc.java
@@ -38,7 +38,7 @@ public class DriveTrainCalc {
 	
 	static double[] processAccelrometerData(double[] data){
 		//converting g's to f/s^2
-		for(int i = 0; i > 3; i++){
+		for(int i = 0; i < 3; i++){
 			data[i] *= 386.088;
 		}
 		return data;
